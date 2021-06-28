3bd27040e0ebc8ad571d08e8b9f1706c5b874a81
JaissonLorenzi <jaisson.lorenzi@gmail.com>
2021-06-28 09:43:45 -0300

    Add metodo sayHeloyManuTimes

--- src/ExemploGit.java
+++ src/ExemploGit.java
@@ -1,10 +1,15 @@
 
 public class ExemploGit {
+	public static void sayHellowManyTimes(int times) {
+		for (int i = 0; i < times; i++) {
+			sayHellow();
+		}
+	}
 	public static void sayHellow() {
 		System.out.println("Olá Mundo!");
 	}
 	
 	public static void main(String[] args) {
-		sayHellow();
+		sayHellowManyTimes(5);
 	}
 }
